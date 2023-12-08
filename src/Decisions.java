9ec23f250a5feb56d9ace68439e44313844b3d4e
Philip McCrickard <pcricket10@gmail.com>
2023-12-08 16:40:29 -0500

    decisions finished

--- src/Decisions.java
+++ src/Decisions.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Decisions {
     public static void main(String[] args) {
 //        if (true) {
@@ -79,26 +81,77 @@ public class Decisions {
 //        } else {
 //            System.out.println("odd number");
 //        }
-        boolean canFly = true;
-        boolean hasFeathers = false;
+//        boolean canFly = true;
+//        boolean hasFeathers = false;
+//
+//        if(canFly && hasFeathers) {
+//            System.out.println("likely a bird");
+//        } else {
+//            System.out.println("may still fly or have feathers, but definitely not both");
+//            if (canFly) {
+//                System.out.println("a rocket?");
+//                System.out.println("time?");
+//            } else {
+//                System.out.println("an ostrich?");
+//                System.out.println("galoshes?");
+//            }
+//            if (hasFeathers) {
+//                System.out.println("a penguin?");
+//                System.out.println("a boa?");
+//            }
+//        }
 
-        if(canFly && hasFeathers) {
-            System.out.println("likely a bird");
-        } else {
-            System.out.println("may still fly or have feathers, but definitely not both");
-            if (canFly) {
-                System.out.println("a rocket?");
-                System.out.println("time?");
-            } else {
-                System.out.println("an ostrich?");
-                System.out.println("galoshes?");
-            }
-            if (hasFeathers) {
-                System.out.println("a penguin?");
-                System.out.println("a boa?");
-            }
+//        double packageWeight = 0.55;
+//
+//        if (packageWeight > 100.0) {        // check first condition
+//            System.out.println("too big for standard shipping");
+//        } else if (packageWeight < 1.0) {   // check a second condition
+//            System.out.println("too small. send a letter.");
+//        } else {                            // the `else` clause executes if all other conditions are false.
+//            System.out.println("can ship");
+//        }
+//        int place = 2;
+//        String ribbonColor;
+//        // 1. switch keyword, then an expression (often a variable) in parentheses
+//        switch (place) {
+//            case 1: // 2. case keyword, then a literal value, then colon.
+//                    // if the switch's expression resolves to the case's value,
+//                    // all code nested inside the case executes.
+//                ribbonColor = "blue";
+//                System.out.println("first place!");
+//                break;  // 3. break keyword, immediately exits the switch's block
+//            case 2:
+//                ribbonColor = "red";
+//                System.out.println("second place!");
+//                break;
+//            case 3:
+//                ribbonColor = "white";
+//                System.out.println("third place!");
+//                break;
+//            default:
+//                ribbonColor = "unknown";
+//        }
+//        System.out.println(ribbonColor);
+//        Scanner console = new Scanner(System.in);
+//        System.out.println("Enter your patron level: ");
+//        String patronLevel = console.nextLine()
+//                .trim()
+//                .toLowerCase();
+        String color = "orange";
+        switch (color) {
+            case "red":
+            case "yellow":
+            case "blue":
+                System.out.println("primary color");
+                break;
+            case "green":
+            case "purple":
+            case "orange":
+                System.out.println("secondary color");
+                break;
+            default:
+                System.out.println("regular color");
         }
-
 
     }
 
